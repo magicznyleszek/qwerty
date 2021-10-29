e0a354eeeb670f43b491537b4f56390707849592
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2021-10-30 00:00:51 +0100

    cleanup comments

--- dye/dye.js
+++ dye/dye.js
@@ -11,17 +11,13 @@
   const ML_TO_CUP = 1/250;
 
   // how much ml water is needed for 1 gram of fabric
-  const WATER_RATIO = 24.2; // 7.5;
-  // Rit says you should use 2.5x, but it's crazy, as you'd never be able to
-  // submerge whole fabric in it.
+  const WATER_RATIO = 24.2;
 
   // how much ml dye is needed for 1 gram of fabric
   const DYE_RATIO = 0.26;
-  // Rit says you should use x0.033, but again: crazy AF.
 
   // how much salt is needed for 1 gram of fabric
   const SALT_RATIO = 1.452;
-  // Same story: Rit crazily suggest x0.16.
 
   const nodes = {};
   nodes.fabricWeight = document.getElementById("fabric-weight");
